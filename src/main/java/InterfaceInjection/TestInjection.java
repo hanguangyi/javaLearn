dc9cb5fd1897b9579851005a23cb1d6224ffd4e8
hanguangyi <hanguangyi@xiaomi.com>
2018-07-23 18:18:54 +0800

    feat(Interface inject): Add interface inject of Person and Wedding of interfaceInjection faction

---
+++ src/main/java/InterfaceInjection/TestInjection.java
@@ -0,0 +1,49 @@
+package InterfaceInjection;
+/**
+ * Copyright (c) 2018/7/23. XiaoMi Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @Author: Han Guangyi
+ * @Mail: hanguangyi@xiaomi.com
+ * @Date: 2018/7/23 下午3:44
+ */
+
+import javafx.application.Application;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jca.context.SpringContextResourceAdapter;
+import org.springframework.stereotype.Component;
+
+/**
+ *@Brief: 简要说明.
+ *
+ *@Detail: 详细注释.
+ *
+ *@Note:
+ */
+//@Configuration
+@ComponentScan
+public class TestInjection {
+
+    public static void main(String[] args){
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(TestInjection.class);
+
+        PersonTool personTool = ctx.getBean(PersonTool.class);
+        int age = personTool.getNewAge();
+        String name = personTool.getNewName("bill");
+        System.out.println("age: "+age+" name: "+name);
+
+    }
+}
